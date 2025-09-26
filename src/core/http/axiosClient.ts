945c87478366e0937e659f14992a3e4883eda386
DESKTOP-0RF1M20\Usuario <j.pita.controlsgroups@gmail.com>
2025-09-26 16:54:04 -0400

    add env config

--- src/core/http/axiosClient.ts
+++ src/core/http/axiosClient.ts
@@ -1,17 +1,15 @@
 import axios from "axios";
-import { useAuthStore } from "../../auth/store/auth.store";
 
 export const api = axios.create({
   baseURL: "https://the-one-api.dev/v2",
 });
 
 api.interceptors.request.use((config) => {
-  const storeToken = useAuthStore.getState().token;
   const envToken = process.env.REACT_APP_ONE_API_TOKEN;
-  const token = storeToken || envToken;
+  const token = envToken;
   if (token) {
     config.headers = config.headers ?? {};
-    (config.headers as any)["Authorization"] = `Bearer -rSTp9SJXwqJQM5bhsvI`;
+    (config.headers as any)["Authorization"] = `Bearer ${token}`;
   }
   return config;
 });
